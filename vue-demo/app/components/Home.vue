11132a5fc0ee5d2399feb24c4a31c11e72fd85e3
Igor Randjelovic <rigor789@gmail.com>
2021-11-02 18:25:44 +0100

    refactor: remove extra StackLayout

--- vue-demo/app/components/Home.vue
+++ vue-demo/app/components/Home.vue
@@ -1,44 +1,43 @@
 <template>
   <Page>
     <ActionBar title="NativeFlix" />
-    <StackLayout height="100%">
-      <ListView
-          height="100%"
-          separatorColor="transparent"
-          for="item in flicks"
-          @itemTap="onFlickTap">
-          <v-template>
-              <GridLayout
-                  height="280"
-                  borderRadius="10"
-                  class="bg-secondary"
-                  rows="*, auto, auto"
-                  columns="*"
-                  margin="5 10"
-                  padding="0">
-                <Image
-                  row="0"
-                  margin="0"
-                  stretch="aspectFill"
-                  :src="item.image"></Image>
-                <Label
-                  row="1"
-                  margin="10 10 0 10"
-                  fontWeight="700"
-                  class="text-primary"
-                  fontSize="18"
-                  :text="item.title"></Label>
-                <Label
-                  row="2"
-                  margin="0 10 10 10"
-                  class="text-secondary"
-                  fontSize="14"
-                  textWrap="true"
-                  :text="item.description"></Label>
-              </GridLayout>
-            </v-template>
-      </ListView>
-    </StackLayout>
+    
+    <ListView
+        height="100%"
+        separatorColor="transparent"
+        for="item in flicks"
+        @itemTap="onFlickTap">
+        <v-template>
+            <GridLayout
+                height="280"
+                borderRadius="10"
+                class="bg-secondary"
+                rows="*, auto, auto"
+                columns="*"
+                margin="5 10"
+                padding="0">
+              <Image
+                row="0"
+                margin="0"
+                stretch="aspectFill"
+                :src="item.image"></Image>
+              <Label
+                row="1"
+                margin="10 10 0 10"
+                fontWeight="700"
+                class="text-primary"
+                fontSize="18"
+                :text="item.title"></Label>
+              <Label
+                row="2"
+                margin="0 10 10 10"
+                class="text-secondary"
+                fontSize="14"
+                textWrap="true"
+                :text="item.description"></Label>
+            </GridLayout>
+          </v-template>
+    </ListView>
   </Page>
 </template>
 
